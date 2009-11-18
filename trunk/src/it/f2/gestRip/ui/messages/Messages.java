80fb216eae478e1cff42afd6d48766af5eb43d64
fferro <fferro@f5145595-3022-4243-8697-0f5c6321aff3>
2009-11-18 15:49:20 +0000

    Spanish language complete

--- trunk/src/it/f2/gestRip/ui/messages/Messages.java
+++ trunk/src/it/f2/gestRip/ui/messages/Messages.java
@@ -8,14 +8,14 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	
+
 	private static final String BUNDLE_NAME_it = "it.f2.gestRip.ui.messages.Messages_it"; //$NON-NLS-1$
 	private static final String BUNDLE_NAME_en = "it.f2.gestRip.ui.messages.Messages_en"; //$NON-NLS-1$
 	private static final String BUNDLE_NAME_nl = "it.f2.gestRip.ui.messages.Messages_nl"; //$NON-NLS-1$
 	private static final String BUNDLE_NAME_es = "it.f2.gestRip.ui.messages.Messages_es"; //$NON-NLS-1$
 
 	private static ResourceBundle RESOURCE_BUNDLE = null;
-	
+
 	public static final ResourceBundle getRESOURCE_BUNDLE(){
 		if(RESOURCE_BUNDLE==null){
 			//System.out.println("Country:"+Locale.getDefault().getCountry());
@@ -30,9 +30,9 @@ public class Messages {
 			}else if(language.equals("nl")){
 				RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME_nl,new Locale("nl","NL"));
 				Locale.setDefault(new Locale("nl","NL"));
-			}else if(language.equals("ru")){
+			}else if(language.equals("es")){
 				RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME_es,new Locale("es","ES"));
-				Locale.setDefault(new Locale("ru","RU"));
+				Locale.setDefault(new Locale("es","ES"));
 			}else {
 				RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME_en,Locale.ENGLISH);
 				Locale.setDefault(Locale.ENGLISH);
@@ -40,7 +40,7 @@ public class Messages {
 		}
 		return RESOURCE_BUNDLE;
 	}
-	
+
 	public static HashMap<String, String> getLanguageMaps() {
 		HashMap<String, String> result = new HashMap<String, String>();
 		result.put("en", "English");
