47f063ce3e23087598289aa37883177db85578af
fferro <fferro@f5145595-3022-4243-8697-0f5c6321aff3>
2010-11-05 11:24:17 +0000

    Check for update funtion complete

--- trunk/src/it/f2/gestRip/EnvConstants.java
+++ trunk/src/it/f2/gestRip/EnvConstants.java
@@ -2,11 +2,12 @@ package it.f2.gestRip;
 
 public class EnvConstants {
 	
-	public static String LAST_REVISION_CHECK_URL = "http://repairslab.sourceforge.net/repairsLabLastRelease.txt";
+	public static String LAST_REVISION_CHECK_URL = "http://repairslab.sourceforge.net/version.properties";
 	public static String LINK_DONATE_IT = "http://repairslab.sourceforge.net/donationIt.php";
 	public static String LINK_DONATE_EN = "http://repairslab.sourceforge.net/donationEn.php";
 	public static String LINK_SUPPORT_REQUEST = "http://sourceforge.net/tracker2/?atid=1116456&group_id=241576&func=browse";
 	public static String LINK_WEBSITE = "http://repairslab.sourceforge.net/";
 	public static String LINK_BUG_REPORT = "http://sourceforge.net/tracker2/?group_id=241576&atid=1116455";
+	public static String LINK_DOWNLOAD = "https://sourceforge.net/projects/repairslab/files/";
 
 }
