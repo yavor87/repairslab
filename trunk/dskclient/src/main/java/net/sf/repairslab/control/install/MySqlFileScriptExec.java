7d97261a5c22acdca47590be9ceea9827edbdf12
fferro <fferro@f5145595-3022-4243-8697-0f5c6321aff3>
2011-08-26 15:02:39 +0000

    Ticket #28 - istallation wizard

--- trunk/dskclient/src/main/java/net/sf/repairslab/control/install/MySqlFileScriptExec.java
+++ trunk/dskclient/src/main/java/net/sf/repairslab/control/install/MySqlFileScriptExec.java
@@ -5,27 +5,42 @@ import net.sf.repairslab.EnvProperties;
 import org.apache.log4j.Logger;
 
 /**
- * TODO Comment for class "MySql" must be completed
+ * MySql sql file instructions
  * @author Fabrizio Ferraiuolo
- * 22/ago/2011
- * 11:18:49
+ * 24/ago/2011
+ * 16:11:45
  */
 public class MySqlFileScriptExec extends AbstractFileScriptExec {
 	
-	
+	/**
+	 * Costruttore della classe MySqlFileScriptExec.java
+	 * @author Fabrizio Ferraiuolo
+	 * 24/ago/2011
+	 * 16:12:16
+	 */
 	public MySqlFileScriptExec() {
 		
 	}
 	
+	/**
+	 * @see net.sf.repairslab.control.install.AbstractFileScriptExec#getInstallFile()
+	 */
 	@Override
     public String getInstallFile() {
 	    return "net/sf/repairslab/install/mysql/install.sql";
     }
+	
+	/**
+	 * @see net.sf.repairslab.control.install.AbstractFileScriptExec#getLogger()
+	 */
 	@Override
     public Logger getLogger() {
 	    return Logger.getLogger(MySqlFileScriptExec.class.getName());
     }
 
+	/**
+	 * @see net.sf.repairslab.control.install.AbstractFileScriptExec#getPrefix()
+	 */
 	@Override
     public String getPrefix() {
 	    return EnvProperties.getInstance().getProperty(EnvProperties.DB_TABLE_PREFIX);
--- trunk/dskclient/src/main/resources/net/sf/repairslab/install/mysql/install.sql
+++ trunk/dskclient/src/main/resources/net/sf/repairslab/install/mysql/install.sql
@@ -1,99 +1,99 @@
 CREATE TABLE &PREF&_tipoapparecchiature (
   id INTEGER NOT NULL,
-  nome VARCHAR(50),
-  descrizione VARCHAR(200),
+  nome VARCHAR(200),
+  descrizione VARCHAR(4000),
   flagAttivo VARCHAR(1),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_schede (
   id INTEGER NOT NULL,
   idCliente INTEGER,
   idStato INTEGER,
   idTipoRip INTEGER,
-  accessoriConsegnati VARCHAR(300),
-  statoGenerale VARCHAR(300),
-  difettoSegnalato VARCHAR(300),
-  nonConform VARCHAR(300),
-  descrizioneRiparazione VARCHAR(300),
-  noteStampa VARCHAR(300),
-  noteUsoInterno VARCHAR(300),
+  accessoriConsegnati VARCHAR(2500),
+  statoGenerale VARCHAR(2500),
+  difettoSegnalato VARCHAR(2500),
+  nonConform VARCHAR(2500),
+  descrizioneRiparazione VARCHAR(3000),
+  noteStampa VARCHAR(2500),
+  noteUsoInterno VARCHAR(2500),
   costoPreventivato FLOAT,
   costoInterno FLOAT,
   pagatoDalCliente FLOAT,
   dataInserimento DATE,
   dataChiusura DATE,
-  descApparecchio VARCHAR(200),
-  imei VARCHAR(30),
-  serial VARCHAR(30),
+  descApparecchio VARCHAR(2500),
+  imei VARCHAR(200),
+  serial VARCHAR(200),
   idTipoApparecchiatura INTEGER,
   idModello INTEGER,
   idMarca INTEGER,
-  numeroDatiAcq VARCHAR(100),
+  numeroDatiAcq VARCHAR(200),
   dataDatiAcq DATE,
   idTipoDatiAcq INTEGER,
   deleted VARCHAR(1),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_tpodatiacquisto (
   id INTEGER NOT NULL,
-  tipo VARCHAR(45),
+  tipo VARCHAR(200),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_tiporiparazione (
   id INTEGER NOT NULL,
-  nomeTipoRip VARCHAR(50),
-  descTipoRip VARCHAR(200),
+  nomeTipoRip VARCHAR(200),
+  descTipoRip VARCHAR(4000),
   flagAttivo VARCHAR(1),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_clienti (
   id INTEGER NOT NULL,
-  nome VARCHAR(50),
-  cognome VARCHAR(50),
-  pIva VARCHAR(50),
-  azienda VARCHAR(15),
-  phone VARCHAR(30),
-  mobilePhone VARCHAR(30),
-  email VARCHAR(50),
-  indirizzo VARCHAR(100),
-  city VARCHAR(50),
+  nome VARCHAR(200),
+  cognome VARCHAR(200),
+  pIva VARCHAR(200),
+  azienda VARCHAR(200),
+  phone VARCHAR(200),
+  mobilePhone VARCHAR(200),
+  email VARCHAR(200),
+  indirizzo VARCHAR(200),
+  city VARCHAR(200),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_anastati (
   id INTEGER NOT NULL,
-  nomeStato VARCHAR(50),
-  descStato VARCHAR(200),
+  nomeStato VARCHAR(200),
+  descStato VARCHAR(4000),
   flagAttivo VARCHAR(1),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_modelli (
   id INTEGER NOT NULL,
-  nome VARCHAR(50),
-  descModello VARCHAR(200),
+  nome VARCHAR(200),
+  descModello VARCHAR(4000),
   flagAttivo VARCHAR(1),
   idMarchi INTEGER,
   idTipoApp INTEGER,
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_marchi (
   id INTEGER NOT NULL,
-  nome VARCHAR(50),
-  descrizione VARCHAR(200),
+  nome VARCHAR(200),
+  descrizione VARCHAR(4000),
   flagAttivo VARCHAR(1),
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE &PREF&_info (
   id VARCHAR(100) NOT NULL,
   value VARCHAR(300) NOT NULL,
   PRIMARY KEY(id)
-);
+) ENGINE = InnoDB;
 
 INSERT INTO &PREF&_info (id, value) VALUES ('metadata_version', '1.0.6');
